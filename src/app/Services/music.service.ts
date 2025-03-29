d92342e996d24aa8728e15f06a31f18c7a56ccc3
Dima Senyk <senikdima12@gmail.com>
2025-03-29 12:57:23 +0000

    MusicService

---
+++ src/app/Services/music.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MusicService {
+
+  constructor(private httpMusic: HttpClient) { }
+
+  getPlayList(): Observable<any>{
+    return this.httpMusic.get('');
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit} from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput } from '@ionic/angular/standalone';
+import { MusicService } from '../Services/music.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, IonInput, IonItem, IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit{
+  constructor(private musicService:MusicService) {}
+
+  Music:any[]=[];
+
+  ngOnInit():void{
+    this.musicService.getPlayList().subscribe((data)=>{
+      this.Music = data.results;
+    });
+  }
+  
 }
