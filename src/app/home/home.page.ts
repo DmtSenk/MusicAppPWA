2d9d7d9e592b6a4040cf3cd1d1d528d61dd0a200
Dima Senyk <senikdima12@gmail.com>
2025-03-30 18:53:11 +0100

    Music service,Tabs,Icon,SearchBar implementation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonSearchbar, IonItem } from '@ionic/angular/standalone';
 import { MusicService } from '../Services/music.service';
 import { CommonModule } from '@angular/common';
 
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [CommonModule, IonInput, IonItem, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonItem, IonLabel, IonIcon, IonTabButton, IonTabBar, IonTabs, IonButton, CommonModule, IonHeader, IonToolbar, IonTitle, IonContent,IonSearchbar ],
 })
 export class HomePage implements OnInit{
   constructor(private musicService:MusicService) {}
